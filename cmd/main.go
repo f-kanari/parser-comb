0b6c758b26e787beadfafa76ca7544124fc29e98
f-kanari <f-kanari@safie.jp>
2025-03-07 16:33:13 +0900

    add parser fn

--- cmd/main.go
+++ cmd/main.go
@@ -6,9 +6,19 @@ import (
 )
 
 func main() {
-	input := "++++xxxx"
-	p := parser.Literal("+")
-	repeated := parser.Repeat(p)
-	ret, err := repeated.Parse(input)
-	fmt.Printf("ret=%s, err=%v", ret, err)
+	parsed := []any{}
+	input := "xxx10"
+	literal := parser.Literal("xxx")
+	digit := parser.Digit()
+	r1, err := literal(input)
+	if err != nil {
+		fmt.Printf("failed to parse: %v\n", err)
+	}
+	parsed = append(parsed, r1.Parsed)
+	r2, err := digit(r1.Rest)
+	if err != nil {
+		fmt.Printf("failed to parse: %v\n", err)
+	}
+	parsed = append(parsed, r2.Parsed)
+	fmt.Println(parsed)
 }
